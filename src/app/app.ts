855a64a16e5ce360a78454a3e5f10e736aae7adc
Anukul20 <akulkarni078@gmail.com>
2026-03-11 14:48:16 +0000

    signal, computed used, lunch items adding and removing done

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,29 @@
-import { Component, signal } from '@angular/core';
+import { Component, computed, signal } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [FormsModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('lunch-planner');
+ lunchItems=signal([""]);
+//when I add new Item, it should reflect in lunchItems
+ newItem=""
+ constructor(){
+  this.lunchItems.set([])
+ }
+ //computing length directly in html, did not use this line
+ totalItems=computed(()=>this.lunchItems.length)
+addItemToList(){
+  this.lunchItems.update(oldvalues => [...oldvalues,this.newItem]);
+  this.newItem=""
+  
+}
+deleteLunchItem(index:number){
+  this.lunchItems.update(newvalues=>newvalues.filter((_,i)=>i!=index))
+}
 }
